400441a79f5f89f61e89466db6c20d0016913f0c
darwinquezada <32519109+darwinquezada@users.noreply.github.com>
2020-09-13 22:59:41 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/usermservice.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/usermservice.dir/main.cpp.o"
+  "usermservice"
+  "usermservice.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/usermservice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
